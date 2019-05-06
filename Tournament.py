d65c8252c238afa16c6be91aa99805fc50f3bb83
Jenna Jordan <jenna.jrdn@gmail.com>
2019-05-06 16:03:32 -0500

    added quickstart
    reorganized files and created quickstart guide in a jupyter notebook

--- Tournament.py
+++ Tournament.py
@@ -1,6 +1,17 @@
+import random
+import pandas as pd
+
 import Game
 import Player
 import Strategy
+
+
+# a list of all strategies, to be used to build a player's list
+allStrategies = [Strategy.AlwaysCooperate, Strategy.AlwaysDefect, Strategy.Random, Strategy.TitForTat,
+                 Strategy.TitForTwoTats, Strategy.TwoTitsForTat, Strategy.FirmButFair,
+                 Strategy.SuspiciousTitForTat, Strategy.HardTitForTat, Strategy.ReverseTitForTat,
+                 Strategy.GrimTrigger, Strategy.SoftGrudger, Strategy.Gradual, Strategy.Pavlov,
+                 Strategy.SoftMajority, Strategy.HardMajority]
 
 
 # function to create player list out of how many times each strategy should appear
@@ -25,7 +36,28 @@ def create_playerlist(strategies: dict):
     return players
 
 
-# function to play tournament and generate list of all game objects
+# function to create player list based on list of strategies
+def create_randomized_playerlist(strategies: list, min: int, max: int):
+    """
+    Generate a list of players given a list of strategies,
+    where each strategy will occur some number of times between the min and max
+    :param strategies: a list of strategies to be used
+    :param min: the minimum number of times the strategy will occur in the player list
+    :param max: the maximum number of times the strategy will occur in the player list
+    :return: a dictionary player list that can be used by the play_tournament function
+    """
+    # create dictionary of form {Strategy.strategyname: #PlayersPerStrategy} from strategy list
+    strategydict = {}
+    for strategy in strategies:
+        strategydict[strategy] = random.randint(min, max)
+
+    # create dictionary of form {playername: playerObject} from strategy dictionary
+    playerlist = create_playerlist(strategydict)
+
+    return playerlist
+
+
+# function to play tournament and generate list of all player stats for each game
 def play_tournament(players, numrounds=100, noisegrowth=0.01, noisemax=0.5, mode='I'):
     """
     Play a tournament in which each player plays a game against every other player.
@@ -89,3 +121,41 @@ def play_tournament(players, numrounds=100, noisegrowth=0.01, noisemax=0.5, mode
         player.ties = 0
 
     return allplayers
+
+
+def run_MCsim(players: dict, times=1000, filename: str = None, numrounds=100, noisegrowth=0.01, noisemax=0.5, mode='I'):
+    """
+    run the monte carlo simulation and export the results to a csv file
+    :param players: players that will be in each tournament
+    :param times: how many times the tournament will be run
+    :param filename: name of the csv file results will be exported to (in Results folder) - don't include '.csv'.
+                     If a filename is not specified, it will return the pandas dataframe.
+    :param numrounds: number of rounds played in each game
+    :param noisegrowth: max noise increment
+    :param noisemax: max noise per game
+    :param mode: game calculates scores based on real plays (misperception, 'P') or noisy plays (misimplementation, 'I')
+    """
+    tCount = 0
+    AllTournamentStats = []
+    for t in range(times):
+        tCount += 1
+        tournament = play_tournament(players, numrounds, noisegrowth, noisemax, mode)
+
+        for player in tournament:
+            tstats = {}
+            tstats['TournamentID'] = tCount
+            tstats['PlayerID'] = player['id']
+            tstats['PlayerStrategy'] = player['strategy']
+            tstats['PlayerScore'] = player['scoreAvg']
+            tstats['PlayerWinRate'] = player['winRate']
+            tstats['PlayerLossRate'] = player['lossRate']
+            tstats['PlayerTieRate'] = player['tieRate']
+            AllTournamentStats.append(tstats)
+
+    results = pd.DataFrame(AllTournamentStats)
+    results = results[['TournamentID', 'PlayerID', 'PlayerStrategy', 'PlayerScore', 'PlayerWinRate', 'PlayerLossRate', 'PlayerTieRate']]
+
+    if filename is None:
+        return results
+    else:
+        results.to_csv('Results/' + filename + '.csv', index=False)
